01d77fbc90a6b6a1881883cb8bca7f509701c05e
Vitor-Kemuel <kemuvitor@gmail.com>
2022-10-20 16:05:13 -0300

    Adicionando component de erro

---
+++ lib/Components/Error/error.component.dart
@@ -0,0 +1,40 @@
+import 'package:burguer/Components/AppBar/app_bar_custom.component.dart';
+import 'package:flutter/material.dart';
+
+class ErrorPage extends StatelessWidget {
+  const ErrorPage({
+    Key? key,
+    required this.stringError,
+  }) : super(key: key);
+  final String stringError;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 109, 109, 109),
+      appBar: const AppBarCustom(
+        title: "Error",
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(20, 40, 20, 40),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.red,
+              style: BorderStyle.solid,
+              width: 2,
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(15),
+            ),
+          ),
+          child: Text(
+            stringError,
+            style: const TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
