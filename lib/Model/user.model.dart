63f6caac327283f1b1d4e78519ae77d3370307d2
Vitor-Kemuel <kemuvitor@gmail.com>
2022-10-20 15:59:49 -0300

    Adiconando admin para o user

--- lib/Model/user.model.dart
+++ lib/Model/user.model.dart
@@ -2,16 +2,19 @@ class UserModel {
   String? uid;
   String? name;
   String? cellphone;
+  bool? admin;
 
   UserModel({
     required this.name,
     required this.uid,
     required this.cellphone,
+    required this.admin,
   });
 
   UserModel.fromMap(Map<String, dynamic> map) {
     name = map['name'];
     uid = map['uid'];
     cellphone = map["cellphone"];
+    admin = map["admin"];
   }
 }
