15c7548a283ed75e89e565d2e1c26a1f2b90073a
Vitor-Kemuel <kemuvitor@gmail.com>
2022-10-22 02:33:01 -0300

    Adicionando botão de navegação nas paginas principais

--- lib/View/Home/home.view.dart
+++ lib/View/Home/home.view.dart
@@ -10,6 +10,9 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final PageController _pageController = PageController();
+  int indexNavigatorBar = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,11 +25,62 @@ class _HomeViewState extends State<HomeView> {
         context: context,
         isDrawer: true,
       ),
-      body: ListView(children: [
-        Center(
-          child: Text("home"),
+      body: PageView(
+        onPageChanged: (int page) {
+          setState(() {
+            indexNavigatorBar = page;
+          });
+        },
+        controller: _pageController,
+        children: [
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+          Container(
+            color: Colors.blue,
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.fromLTRB(15, 15, 15, 10),
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 0, 0, 0),
         ),
-      ]),
+        child: BottomNavigationBar(
+          onTap: (int page) {
+            setState(() {
+              indexNavigatorBar = page;
+            });
+            _pageController.jumpToPage(
+              page,
+            );
+          },
+          currentIndex: indexNavigatorBar,
+          elevation: 0,
+          backgroundColor: const Color.fromARGB(0, 0, 0, 0),
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          fixedColor: Colors.red,
+          unselectedItemColor: Colors.white,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.fastfood_outlined),
+              label: "Laches",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.icecream_outlined),
+              label: "Sobremesa",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.local_drink_rounded),
+              label: "Bebidas",
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
